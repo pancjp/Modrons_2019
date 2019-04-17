f6f253f126f35ef9ebcb64003832cc53b0b21850
NameClassified <cjp.pan@gmail.com>
2019-04-16 19:55:06 -0500

    initial commit

---
+++ robot_bluetooth/robot_bluetooth.ino
@@ -0,0 +1,77 @@
+#include <BluetoothSerial.h>
+#include "BluetoothSerial.h"
+#include <stdio.h>
+#include <math.h>
+
+BluetoothSerial SerialBT;
+
+int count = 0;
+const int PWM1pin = 14;
+const int DIR1pin = 32;
+const int PWM1channel = 0;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("A R N I E"); //Bluetooth device name
+  //Serial.println("The device started, now you can pair it with bluetooth!");
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+
+  ledcSetup(PWM1channel,5000,8); // pwm channel, frequency, resolution in bits
+  ledcAttachPin(PWM1pin,PWM1channel); // pin, pwm channel
+
+  pinMode(DIR1pin,OUTPUT); // set direction pin to output
+
+  ledcWrite(PWM1channel,0); // pwm channel, speed 0-255
+  digitalWrite(DIR1pin, LOW); // set direction to cw/ccw
+}
+
+void loop() {
+  char c = 0;
+  int an = 0;
+  int i;
+  
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    //Serial.write(SerialBT.read());
+    c = SerialBT.read();
+    if(c == 'o'){
+      digitalWrite(13,HIGH);
+    }
+    else if (c == 'p'){
+      digitalWrite(13,LOW);
+    }
+    else if (c == 'a'){
+      an = analogRead(A2);
+      SerialBT.print("a ");
+      SerialBT.println(an);
+    }
+    else if (c == 'x'){
+      count = count + 1;
+      for (i=0; i<100; i++){
+        SerialBT.print("x ");
+        SerialBT.print(i);
+        SerialBT.print(" ");
+        SerialBT.println(int(50*sin(2*3.14/50*i+count)));
+      }
+    }
+    else if (c == 'c') {
+      SerialBT.println("c 1");
+    }
+
+    else if (c == 'w') {
+      digitalWrite(DIR1pin, LOW);
+      ledcWrite(PWM1channel,100);
+    }
+    else if (c == 's') {
+      digitalWrite(DIR1pin, HIGH);
+      ledcWrite(PWM1channel, 100);
+    }
+    else if (c == 'b') {
+      ledcWrite(PWM1channel, 0);
+    }
+  }
+  delay(20);
+}
---
+++ robot_bluetooth_receiver.py
@@ -0,0 +1,59 @@
+import serial
+import string
+
+WIDTH = 500
+HEIGHT = 250
+TITLE = "Fading Green!"
+c = 0
+x = [0]*100 # list of size 100 initialized to 0
+analog = 0
+
+def draw():
+    screen.fill((0, c, 0))
+    plot(x,10,HEIGHT/2,0,0,255) # data, xpos, ypos, red, green, blue
+    screen.draw.text(str(analog), (250, 20), color="orange", fontsize=80) # top left to bottom right, ypos is inverted
+
+def plot(data,xpos,ypos,r,g,b):
+    ypos = HEIGHT - ypos # flip the y axis
+    for i in range(1, len(data)):
+        screen.draw.line((xpos+i,ypos),(xpos+i,ypos-x[i]),(r,g,b))
+
+def update(dt):
+    global c, HEIGHT
+    global x, analog
+    c = (c + 1) % 256
+    while ser.in_waiting:
+        line = ser.read_until().strip() #strip() removes the \r\n
+        values = line.decode('ascii').split(' ')
+        #print(values)
+        if(values[0] == 'x'):
+            x[int(values[1])] = int(values[2])
+            #print(x)
+        if(values[0] == 'a'):
+            analog = int(values[1])
+
+def on_mouse_down(button, pos):
+    #print("Mouse button", button, "down at", pos)
+    ser.write(b'o')
+
+def on_mouse_up(button, pos):
+    #print("Mouse button", button, "up at", pos)
+    ser.write(b'p')
+    ser.write(b'a')
+
+def on_key_down(key): #key names are saved in CAPS
+    if key.name == 'X':
+        ser.write(b'x')
+        #print("Sent x")
+    if key.name == 'C':
+        ser.write(b'c')
+        #print("Sent c")
+    if key.name == 'W':
+        ser.write(b'w')
+    if key.name == 'S':
+        ser.write(b's')
+    if key.name == 'B':
+        ser.write(b'b')
+
+
+ser = serial.Serial('/dev/cu.SLAB_USBtoUART',9600)
